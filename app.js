e1d89c79ed1856568101ad7dcf9f30fc4f9452a8
Kanan Farzaliyev <kanan.farzali@gmail.com>
2025-03-08 17:35:12 +0100

    implement arcjet middleware

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ import { PORT } from './config/env.js'
 
 import cookieParser from 'cookie-parser'
 import connectToDatabase from './database/mongodb.js'
+import arcjetMiddleware from './middlewares/arcjet.middleware.js'
 import errorMiddleware from './middlewares/error.middleware.js'
 import authRouter from './routes/auth.routes.js'
 import subscriptionRouter from './routes/subscription.routes.js'
@@ -13,6 +14,7 @@ const app = express()
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
+app.use(arcjetMiddleware)
 
 app.use('/api/v1/auth', authRouter)
 app.use('/api/v1/users', userRouter)
