29804b9f1e9bee2263413d34e1abcbaed7f688ca
Kanan Farzaliyev <kanan.farzali@gmail.com>
2025-03-08 16:48:31 +0100

    implement getUsers and getUserById functionalities

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,10 +1,11 @@
 import { Router } from 'express'
+import { getUser, getUsers } from '../controllers/user.controller.js'
 
 const userRouter = Router()
 
-userRouter.get('/', (req, res) => res.send({ title: 'Get all users' }))
+userRouter.get('/', getUsers)
 
-userRouter.get('/:id', (req, res) => res.send({ title: 'Get user details' }))
+userRouter.get('/:id', getUser)
 
 userRouter.post('/', (req, res) => res.send({ title: 'Create a new user' }))
 
